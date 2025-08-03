a6fcafa87c38c6d84acc4075135a51b649d9e841
mahdieh1992 <mohamadimahdieh70@gmail.com>
2025-08-03 10:16:31 +0330

    Exercise_tuple

---
+++ Exercise_tuple.py
@@ -0,0 +1,10 @@
+# 4.13 Buffet
+buffet = ("Chicken","Pizza","Salad","Pasta")
+for food in buffet:
+    print(food)
+print()
+# buffet[1] = "coffee"
+
+buffet = ("tea","water","Salad","Pasta")
+for food in buffet:
+    print(food)
